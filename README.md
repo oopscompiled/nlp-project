5f0eff52ab2db49db90ad156d24c9c1e2b5685ba
Alex <pacuk@MacBook-Air-Alex.local>
2025-04-05 22:35:40 +0200

    fix regex

--- README.md
+++ README.md
@@ -3,3 +3,18 @@
 MBTI Personality Type Twitter Dataset
 
 Data was given from: https://www.kaggle.com/datasets/mazlumi/mbti-personality-type-twitter-dataset/data
+
+
+nlp_project/
+├── notebooks/
+│   ├── exploratory.ipynb
+│   ├── training.ipynb
+│   └── evaluation.ipynb
+├── src/
+│   ├── model.py
+│   ├── utils.py
+│   └── data_loader.py
+├── requirements.txt
+├── README.md
+├── .gitignore
+└── main.py
--- src/utils.py
+++ src/utils.py
@@ -1,16 +1,20 @@
-import re
+import regex as re
 
 def remove_emojis(data):
     return re.sub(r'\p{Emoji}', ' ', data)
 
 def clean_data(data):
+    if not isinstance(data, str):
+        return ""
+
     data = remove_emojis(data)
     data = data.lower()
 
-    data = re.sub(r"<.*?>", " ", data)              # Remove HTML tags
-    data = re.sub(r"\s?@\w+", " ", data)            # Remove @mentions
-    data = re.sub(r"https?://\S+|www\.\S+", " ", data)  # Remove URLs
-    data = re.sub(r"[^\w\s]", " ", data)            # Remove punctuation/special chars
-    data = " ".join(data.split())                   # Remove extra whitespace
-    
+    data = re.sub(r"<.*?>", " ", data)
+    data = re.sub(r"\s?@\w+", " ", data)
+    data = re.sub(r"https?://[^\s|]+|www\.[^\s|]+", " ", data)  # 👈 улучшено!
+    data = re.sub(r"\|\|+|[-_=~]{2,}", " ", data)
+    data = re.sub(r"[^\w\s]", " ", data)
+    data = " ".join(data.split())
+
     return data
