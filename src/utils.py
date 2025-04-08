8ee27c89140e79f2c547b1393c0941966f55664b
Alex <pacuk@MacBook-Air-Alex.local>
2025-04-08 14:16:57 +0200

    small fix

--- src/utils.py
+++ src/utils.py
@@ -4,45 +4,44 @@
 #     if not isinstance(data, str):
 #         return ""
 
-#     data = re.sub(r'https?://\S+', '', data)             # Удалить ссылки
-#     data = re.sub(r'@\w+', '', data)                     # Удалить @юзернеймы
-#     data = re.sub(r'\|\|+|[-_=~]{2,}', ' ', data)        # Удалить спец. символы
-#     data = re.sub(r'<.*?>', '', data)                    # Удалить HTML-теги
-#     data = re.sub(r'\p{Emoji}', '', data)                # Удалить эмодзи
-#     data = re.sub(r'[^\w\s]', '', data)                  # Удалить всё лишнее кроме слов
-#     data = " ".join(data.split())                        # Привести пробелы в порядок
+#     data = re.sub(r'https?://\S+', '', data)            
+#     data = re.sub(r'@\w+', '', data)                    
+#     data = re.sub(r'\|\|+|[-_=~]{2,}', ' ', data)      
+#     data = re.sub(r'<.*?>', '', data)                    
+#     data = re.sub(r'\p{Emoji}', '', data)               
+#     data = re.sub(r'[^\w\s]', '', data)                 
+#     data = " ".join(data.split())                     
 
 #     return data
 
 import regex as re
-import html  # Для декодирования HTML сущностей
+import html
 
 def clean_data(data):
     if not isinstance(data, str):
         return ""
 
-    # Декодируем HTML-сущности (заменяет &gt;, &amp;, &lt; и другие на символы)
+    data = data.str.lower()
+    # decode html entities
     data = html.unescape(data)
 
-    # Удалить ссылки
     data = re.sub(r'https?://\S+', '', data)
 
-    # Удалить @юзернеймы
     data = re.sub(r'@\w+', '', data)
 
-    # Удалить спец. символы
     data = re.sub(r'\|\|+|[-_=~]{2,}', ' ', data)
 
-    # Удалить HTML-теги
     data = re.sub(r'<.*?>', '', data)
 
-    # Удалить эмодзи (если это необходимо)
     data = re.sub(r'\p{Emoji}', '', data)
 
-    # Удалить всё лишнее кроме слов
     data = re.sub(r'[^\w\s]', '', data)
 
-    # Привести пробелы в порядок (удалить лишние пробелы)
     data = " ".join(data.split())
 
     return data
+
+word_re = re.compile(r"\b[a-zA-Z]{3,}\b", re.IGNORECASE)
+
+def extract_clean_words(text):
+    return word_re.findall(text)
