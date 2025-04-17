1bd5ca8a82f50fbe4eb12c7fff0be5bd396a03cd
Alex <heleveins@gmail.com>
2025-04-17 16:02:01 +0200

    update

--- src/utils.py
+++ src/utils.py
@@ -1,21 +1,28 @@
-import regex as re
+import re
 import html
+from bs4 import BeautifulSoup
 
-# no need in proper cleaning for bert
+
 
 def clean_for_bert(text):
     if not isinstance(text, str):
         return ""
 
     text = html.unescape(text)
-    text = re.sub(r'https?://\S+', '', text)       
-    text = re.sub(r'<.*?>', '', text)              
+    
+    text = BeautifulSoup(text, "html.parser").get_text()
+    
+    text = re.sub(r'https?://\S+', '', text)
+    
+    text = re.sub(r'\b(http|href)\b', '', text, flags=re.IGNORECASE)
+    
+    text = re.sub(r'&\w+;', '', text)
+    text = re.sub(r'[@#]\S+', '', text)
     text = re.sub(r'\s+', ' ', text).strip()
 
     return text
 
-
-word_re = re.compile(r"\b[a-zA-Z0-9]{2,}\b", re.IGNORECASE)
+word_re = re.compile(r"\b[a-zA-Z]{1,}\b", re.IGNORECASE)
 
 def extract_clean_words(text):
     return word_re.findall(text)
