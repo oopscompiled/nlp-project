2a4ecb202d31e6d7eb06601a90514300a77a74e2
Alex <heleveins@gmail.com>
2025-04-17 13:26:54 +0200

    fix

--- src/utils.py
+++ src/utils.py
@@ -1,48 +1,21 @@
-# import regex as re
-
-# def clean_data(data):
-#     if not isinstance(data, str):
-#         return ""
-
-#     data = re.sub(r'https?://\S+', '', data)            
-#     data = re.sub(r'@\w+', '', data)                    
-#     data = re.sub(r'\|\|+|[-_=~]{2,}', ' ', data)      
-#     data = re.sub(r'<.*?>', '', data)                    
-#     data = re.sub(r'\p{Emoji}', '', data)               
-#     data = re.sub(r'[^\w\s]', '', data)                 
-#     data = " ".join(data.split())                     
-
-#     return data
-
 import regex as re
 import html
 
-def clean_data(data):
-    if not isinstance(data, str):
+# no need in proper cleaning for bert
+
+def clean_for_bert(text):
+    if not isinstance(text, str):
         return ""
 
-    # data = data.str.lower()
-    
-    # decode html entities
-    data = html.unescape(data)
+    text = html.unescape(text)
+    text = re.sub(r'https?://\S+', '', text)       
+    text = re.sub(r'<.*?>', '', text)              
+    text = re.sub(r'\s+', ' ', text).strip()
 
-    data = re.sub(r'https?://\S+', '', data)
+    return text
 
-    data = re.sub(r'@\w+', '', data)
 
-    data = re.sub(r'\|\|+|[-_=~]{2,}', ' ', data)
-
-    data = re.sub(r'<.*?>', '', data)
-
-    data = re.sub(r'\p{Emoji}', '', data)
-
-    data = re.sub(r'[^\w\s]', '', data)
-
-    data = " ".join(data.split())
-
-    return data
-
-word_re = re.compile(r"\b[a-zA-Z]{2,}\b", re.IGNORECASE)
+word_re = re.compile(r"\b[a-zA-Z0-9]{2,}\b", re.IGNORECASE)
 
 def extract_clean_words(text):
     return word_re.findall(text)
